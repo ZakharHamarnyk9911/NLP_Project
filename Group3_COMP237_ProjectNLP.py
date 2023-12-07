b94701359d1b0a9a493f328bd9c5cf387991ae50
Zakhar <zhamarny@my.centennialcollege.ca>
2023-12-06 23:34:26 -0500

    Updated code

--- Group3_COMP237_ProjectNLP.py
+++ Group3_COMP237_ProjectNLP.py
@@ -15,6 +15,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+from nltk.corpus import stopwords
+
 #load Data
 filename = "Youtube03-LMFAO.csv"
 path = "C:/Users\zhama\AI assignments\Group3_COMP237_ProjectNLP"
@@ -22,50 +24,63 @@ file_path = os.path.join(path, filename)
 
 group_3 = pd.read_csv(file_path)
 
-#drop colums
-group_3 = group_3.drop(['COMMENT_ID', 'AUTHOR', 'DATE'], axis=1)
-print(group_3)
+
+#data exploration
+print(group_3.head(3))
+print(group_3.columns)
+
+#drop colums and prepare the data for model building
+group_3_to_drop = ['COMMENT_ID', 'AUTHOR', 'DATE']
+training_data = group_3.drop(group_3_to_drop, axis = 1)
+print(training_data)
+
+#shuffle the data
+df_shuffled = training_data.sample(frac = 1, random_state = 1)
+
+#prepare the data
+count_vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
+train_data = count_vectorizer.fit_transform(df_shuffled['CONTENT'])
+
+print(type(train_data))
+print(train_data.shape)
+names = count_vectorizer.get_feature_names_out()
+print(names[:10])
+
+
+#Downscale the transformed data using td-idf
+tfidf = TfidfTransformer()
+train_tfidf = tfidf.fit_transform(train_data)
+
+print(type(train_tfidf))
+print(train_tfidf.shape)
 
 #split date using pandas
 split_data = int(0.75 * len(group_3))
-train_data = group_3[:split_data]
-test_data = group_3[split_data:]
-print(train_data)
-print(test_data)
+X_train , Y_train = train_tfidf[:split_data], df_shuffled['CLASS'][:split_data]
+X_test , Y_test =train_tfidf[:split_data], df_shuffled['CLASS'][:split_data]
+print(X_train,Y_train)
+print(X_test,Y_test)
 
-#seperate the features
-X_train = train_data['CONTENT']
-y_train = train_data['CLASS']
-X_test = test_data['CONTENT']
-y_test = test_data['CLASS']
-#vectorize the training data
-count_vectorizer = CountVectorizer()
-train_tc = count_vectorizer.fit_transform(X_train)
-tfidf_transformer = TfidfTransformer()
-train_tfidf = tfidf_transformer.fit_transform(train_tc)
 
-#vectorize the test data transform only
-test_tc = count_vectorizer.transform(X_test)
-tfidf = tfidf_transformer.transform(test_tc)
 
-#cross-validate the model
+#fit the classifier with the training data
 classifier = MultinomialNB()
-scores = cross_val_score(classifier, train_tfidf, y_train, cv=5)
-print("Accuracy of each fold:", scores)
+classifier.fit(X_train,Y_train)
+#cross-validate the model
+scores = cross_val_score(classifier,X_train , Y_train, cv=5)
+#Test the modle on the test data
 mean_accuracy = np.mean(scores)
 print("Mean Accuracy:", mean_accuracy)
 
-#fit the classifier with the training data
-classifier.fit(train_tfidf, y_train)
 
 #make predictions on the test data
-y_pred = classifier.predict(tfidf)
+pred = classifier.predict(X_test)
 
 #calculataion of the confusion matrix
-calc_confusion_matrix = confusion_matrix(y_test, y_pred)
-print(calc_confusion_matrix)
+calc_confusion_matrix = confusion_matrix(Y_test, pred)
+print("Matirx:", calc_confusion_matrix)
 #calculate accuracy
-accuracy = accuracy_score(y_test,y_pred)
+accuracy = accuracy_score(Y_test,pred)
 print("Accuracy is :", accuracy)
 
 #test our comments
@@ -78,15 +93,16 @@ test_data_comments =[
     "Good job, appreceit it",
     "Really insightful analysis, thanks for sharing!",
     "Your explanation made this topic so clear, great job!",
-    "Hot singles in your area waiting to meet you! Click now!"
+    "Nice video, I love you so much guys",
+    "Congratulations! You've been selected for a free gift card. Click now to claim!",
+    "Boost your followers instantly! Click here for more details.",
+    "This is fantastic! Keep up the amazing job!"
+    
     ]
-input_tc = count_vectorizer.transform(test_data_comments)
-type(input_tc)
-print(input_tc)
 #transform vectorized data using tfidf transformer
-input_tfidf = tfidf_transformer.transform(input_tc)
-type(input_tfidf)
-print(input_tfidf)
+input_tc = count_vectorizer.transform(test_data_comments)
+input_tfidf = tfidf.transform(input_tc)
+
 #predict the output
 predictions = classifier.predict(input_tfidf)
 
